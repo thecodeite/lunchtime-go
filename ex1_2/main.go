92601bf0e7719dcb3145573245d798a08020ab9a
Sam Plews <the@codeite.com>
2016-04-20 12:10:44 +0100

    lesson_1

---
+++ ex1_2/main.go
@@ -0,0 +1,21 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 6.
+//!+
+
+// ex1_2 prints its command-line arguments.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for index, arg := range os.Args[1:] {
+		fmt.Println(index, arg)
+	}
+}
+
+//!-
